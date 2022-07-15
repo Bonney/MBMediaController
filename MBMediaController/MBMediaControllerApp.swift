9045317462acd371715660359b174b1c7169abdd
Matt Bonney <bonneym@gmail.com>
2022-07-14 21:24:22 -0400

    Initial Commit

---
+++ MBMediaController/MBMediaControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBMediaControllerApp.swift
+//  MBMediaController
+//
+//  Created by Matt Bonney on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MBMediaControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
